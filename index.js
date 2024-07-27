2f05e4eadc67d6910830a791114bc386ee01ebbd
AnkitKumarDelta <ankitna355@gmail.com>
2024-07-27 11:53:51 +0530

    save changes

--- index.js
+++ index.js
@@ -10,31 +10,33 @@ const io = new Server(server);
 const users = {};
 
 io.on('connection', (socket) => {
-    //if any new user join  let other user know about this
+    // If a new user joins, let other users know
     socket.on('new-user-joined', (name) => {
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
     });
-// if someone sends a message broadcast all
-socket.on('send', data => {
-    if (data.type === 'message') {
-        socket.broadcast.emit('receive', { type: 'message', data: data.data, name: users[socket.id] });
-    } else if (data.type === 'file') {
-        socket.broadcast.emit('receive', { type: 'file', data: data.data, fileName: data.fileName, name: users[socket.id] });
-    }
-});
-//if someone leave the chat broadcast others
+
+    // If someone sends a message, broadcast it to all
+    socket.on('send', (data) => {
+        if (data.type === 'message') {
+            socket.broadcast.emit('receive', { type: 'message', data: data.data, name: users[socket.id] });
+        } else if (data.type === 'file') {
+            // Broadcast file as binary data
+            socket.broadcast.emit('receive', { type: 'file', data: data.data, fileName: data.fileName, name: users[socket.id] });
+        }
+    });
+
+    // If someone leaves the chat, let others know
     socket.on('disconnect', () => {
-        socket.broadcast.emit('left',users[socket.id]);
+        socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     });
 });
 
-
 app.use(express.static(path.resolve("./public")));
 
 app.get("/", (req, res) => {
-  return res.sendFile(path.join(__dirname, "/public/index.html"));
+    return res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 server.listen(9000, () => console.log(`Server Started at PORT:9000`));
